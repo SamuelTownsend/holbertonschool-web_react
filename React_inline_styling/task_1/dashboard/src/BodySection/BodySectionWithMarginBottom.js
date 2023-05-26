1963c7ca1b03282e6186cc1fbd22dff762763cd0
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-05-26 14:34:32 -0500

    adding body section for task 1

--- React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BodySection from './BodySection';
-import './BodySection.css';
+import { css, StyleSheet } from 'aphrodite';
+
+const styles = StyleSheet.create({
+	bodySectionWithMargin: {
+		marginBottom: '40px'
+	}
+});
 
 export default class BodySectionWithMarginBottom extends React.Component {
 	render () {
 		return (
-			<div className='bodySectionWithMargin'>
+			<div className={`bodySectionWithMargin ${css(styles.bodySectionWithMargin)}`}>
 				<BodySection {...this.props} />
 			</div>
 		);
