2f3dfa4b14e876710c284cced0a910d7ea201c14
Sam Townsend <samtownsend@Seans-iMac.local>
2023-06-05 21:21:36 -0500

    adding task_0

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -1,20 +1,21 @@
 import logo from './holberton-logo.jpg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School Dashboard</h1>
-      </div>
-      <div className="App-body">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>School Dashboard</h1>
+    </header>
+    <body className="App-body">
       <p>Login to access the full dashboard</p>
-      </div>
-      <div className="App-footer">
-        <p>Copyright 2020 - Holberton School</p>
-      </div>
-    </div>
+    </body>
+    <footer className="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </footer>
+  </div>
   );
 }
 
